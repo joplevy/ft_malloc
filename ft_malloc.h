309882802e342bf6f93c2b103d1befc6c0298441
jplevy <jplevy@alfred.home>
2018-09-14 15:02:52 +0200

    ok

--- ft_malloc.h
+++ ft_malloc.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_malloc.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/26 15:39:03 by jplevy            #+#    #+#             */
-/*   Updated: 2018/09/07 13:09:19 by jplevy           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+	/* ************************************************************************** */
+	/*                                                                            */
+	/*                                                        :::      ::::::::   */
+	/*   ft_malloc.h                                        :+:      :+:    :+:   */
+	/*                                                    +:+ +:+         +:+     */
+	/*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
+	/*                                                +#+#+#+#+#+   +#+           */
+	/*   Created: 2018/08/26 15:39:03 by jplevy            #+#    #+#             */
+	/*   Updated: 2018/09/12 15:59:40 by jplevy           ###   ########.fr       */
+	/*                                                                            */
+	/* ************************************************************************** */
 
 #ifndef FT_MALLOC_H
 # define FT_MALLOC_H
@@ -18,9 +18,6 @@
 # include <sys/mman.h>
 # include <unistd.h>
 // # include <pthread.h>
-// # include "libft/libft.h"
-// # include <unistd.h>
-// # include <sys/mman.h>
 // # include <fcntl.h>
 // # define mmap(p1,p2,p3,p4,p5,p6) NULL
 
@@ -33,25 +30,26 @@
 // 	t_list		*other_mapping;
 // }				t_header;
 
-typedef struct	s_arena_container
+typedef struct					s_arena_container
 {
 	t_list						*zones;
+	void						*first;
 	size_t						nb_alloc;
 	size_t						arena_id;
 	struct s_arena_container	*next;
-}				t_arena_container;
+}								t_arena_container;
 
-typedef struct	s_all_infos
+typedef struct					s_all_infos
 {
 	t_arena_container			*tiny_mapping;
 	t_arena_container			*small_mapping;
-	t_list		*other_mapping;
-	size_t		tiny_size;
-	size_t		tiny_node_size;
-	size_t		small_size;
-	size_t		small_node_size;
-	size_t		page_size;
-}				t_all_infos;
+	t_list						*other_mapping;
+	size_t						tiny_node_size;
+	size_t						tiny_size;
+	size_t						small_size;
+	size_t						small_node_size;
+	size_t						page_size;
+}								t_all_infos;
 
 
 extern t_all_infos g_all_infos;
--- main.c
+++ main.c
@@ -6,28 +6,40 @@
 /*   By: jplevy <jplevy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 15:38:59 by jplevy            #+#    #+#             */
-/*   Updated: 2018/09/07 19:30:06 by jplevy           ###   ########.fr       */
+/*   Updated: 2018/09/14 14:41:49 by jplevy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <unistd.h>
-// #include <sys/mman.h>
-// #include <sys/resource.h>
-
-// int main(void)
-// {
-//     // struct rlimit limit;
-//     int limit_ret = getrlimit(RLIMIT_DATA);
-//     void *toto = mmap(0, 4046, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-//     printf("%p, %d\n", toto, limit_ret);
-//     return (0);
-// }
 #include "ft_malloc.h"
+
+t_list				*ft_init_zones(size_t size)
+{
+	t_list	*ret;
+	t_list	*tmp;
+	void	*map;
+	int		i;
+
+	if (!(map = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)) \
+	|| !(ret =(t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
+		return (NULL);
+	i = -1;
+	tmp = ret;
+	while (++i < 128)
+	{
+		tmp->content = map;
+		map += size / 128;
+		tmp->content_size = 0;
+		tmp->next = (i < 127) ? (t_list *)((long)tmp + (long)sizeof(t_list)) : ret;
+		// printf("tmp=%p and ret=%p and content=%p\n", tmp, ret, tmp->content);
+		tmp = tmp->next;
+	}
+	// printf("tmp=%p and ret=%p\n", tmp, ret);
+	return (ret);
+}
 
 t_arena_container	*ft_init_one_map(size_t size)
 {
 	t_arena_container	*cont;
-	// int		i;
 	
 	if (size == g_all_infos.tiny_size || size == g_all_infos.small_size)
 	{
@@ -41,76 +53,13 @@ t_arena_container	*ft_init_one_map(size_t size)
 			cont = (t_arena_container *)((long)cont + (long)sizeof(t_arena_container));
 		}
 	}
-	cont->zones = NULL;
+	if (!(cont->zones = ft_init_zones(size)))
+		return (NULL);
+	cont->first = cont->zones;
 	cont->next = NULL;
 	cont->arena_id = 0;
 	cont->nb_alloc = 0;
 	return (cont);
-	// 		if (!(g_all_infos.tiny_mapping = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = g_all_infos.tiny_mapping;
-	// 	}
-	// 	else
-	// 	{
-	// 		i = 0;
-	// 		while (++i && list->next)
-	// 			list = list->next;
-	// 		if (i == 0 || i != (g_all_infos.page_size / (sizeof(t_list))))
-	// 			list->next = (void*)((long)list + (long)sizeof(t_list));
-	// 		else if (!(list->next = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = list->next;
-	// 	}
-	// }
-	// else if (size == g_all_infos.small_size)
-	// {
-	// 	if (!(list = g_all_infos.small_mapping))
-	// 	{
-	// 		if (!(g_all_infos.small_mapping = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = g_all_infos.small_mapping;
-	// 	}
-	// 	else
-	// 	{
-	// 		i = 0;
-	// 		while (++i && list->next)
-	// 			list = list->next;
-	// 		if (i == 0 || i != (g_all_infos.page_size / (sizeof(t_list))))
-	// 			list->next = (void*)((long)list + (long)sizeof(t_list));
-	// 		else if (!(list->next = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = list->next;
-	// 	}
-	// }
-	// else
-	// {
-	// 	if (!(list = g_all_infos.other_mapping))
-	// 	{
-	// 		if (!(g_all_infos.other_mapping = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = g_all_infos.other_mapping;
-	// 	}
-	// 	else
-	// 	{
-	// 		i = 0;
-	// 		while (++i && list->next)
-	// 			list = list->next;
-	// 		if (i == 0 || i != (g_all_infos.page_size / (sizeof(t_list))))
-	// 			list->next = (void*)((long)list + (long)sizeof(t_list));
-	// 		else if (!(list->next = (t_list *)(mmap(0, g_all_infos.page_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))))
-	// 			return (NULL);
-	// 		list = list->next;
-	// 	}
-	// }
-	// if (!(list->content = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
-	// {
-	// 	printf("cant allocate\n");
-	// 	return (NULL);
-	// }
-	// list->content_size = size;
-	// list->next = NULL;
-	// printf("end\n");	
-	// return (list);
 }
 
 void	ft_init_sizes()
@@ -123,109 +72,92 @@ void	ft_init_sizes()
 	g_all_infos.tiny_node_size = g_all_infos.tiny_size / 128;
 	g_all_infos.small_size = page_size * 32;
 	g_all_infos.small_node_size = g_all_infos.small_size / 128;
-	printf("page_size = %zu\ntiny_size = %zu\ntiny_node_size = %zu\nsmall_size = %zu\nsmall_node_size = %zu\n",\
-	g_all_infos.page_size,
-	g_all_infos.tiny_size,
-	g_all_infos.tiny_node_size,
-	g_all_infos.small_size,
-	g_all_infos.small_node_size
-	);
 }
 
 void 	*ft_malloc(size_t size)
 {
-	// static t_header head = {0, 0, NULL, NULL, NULL, 0, 0, 0, 0, 0};
 	t_arena_container	*cont;
+	void				*ret;
+
 	if (g_all_infos.tiny_size == 0 || g_all_infos.tiny_node_size == 0 \
 		|| g_all_infos.small_size == 0 || g_all_infos.small_node_size == 0)
 		ft_init_sizes();
-	// if (!(g_all_infos))
-	// {
-	// 	if (!(g_all_infos = mmap(0, g_all_infoss.tiny_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
-	// 		return (NULL);
-	// 	g_all_infos->nb_tiny = 0;
-	// 	g_all_infos->nb_small = 0;
-	// }
+
 	if (size <= g_all_infos.tiny_node_size)
 	{
 		if (!(g_all_infos.tiny_mapping) || g_all_infos.tiny_mapping->nb_alloc == 128)
 		{
-			printf("initiatizing tiny sizeof (t_list) is %d\n", (int)sizeof(t_arena_container));
+			// printf("initiatizing tiny sizeof (t_list) is %d\n", (int)sizeof(t_arena_container));
 			if (!(cont = ft_init_one_map(g_all_infos.tiny_size)))
 				return (NULL);
 			cont->next = g_all_infos.tiny_mapping;
 			cont->arena_id = (cont->next) ? cont->next->arena_id + 1 : 1;
-			printf("%p %p %p\n", cont, cont->next, g_all_infos.tiny_mapping);
 			g_all_infos.tiny_mapping = cont;
-			printf("%p %p %p\n", cont, cont->next, g_all_infos.tiny_mapping);
-			
-			// int j = -1;
-			// t_list *li = g_all_infos.tiny_mapping;
-			// while (++j < g_all_infos.nb_tiny / 128)
-			// {
-			// 	// printf("node nb %i at %p is %p\n", j, li, li->content);
-			// 	li = li->next;
-			// }
-			// printf("node nb %i at %p is %p\n", j, li, li->content);
 		}
 		g_all_infos.tiny_mapping->nb_alloc += 1;
-		// printf("give tiny malloc");
-		// g_all_infos.nb_tiny += 1;
-		// printf("loaded\n");
+		g_all_infos.tiny_mapping->zones->content_size = (size > 0) ? size : 1;
+		ret = g_all_infos.tiny_mapping->zones->content;
+		g_all_infos.tiny_mapping->zones = g_all_infos.tiny_mapping->zones->next;
+		return (ret);
 	}
 	else if (size <= g_all_infos.small_node_size)
 	{
 		if (!(g_all_infos.small_mapping) || g_all_infos.small_mapping->nb_alloc == 128)
 		{
-			printf("initiatizing small sizeof (t_list) is %d\n", (int)sizeof(t_arena_container));
+			// printf("initiatizing small sizeof (t_list) is %d\n", (int)sizeof(t_arena_container));
 			if (!(cont = ft_init_one_map(g_all_infos.small_size)))
 				return (NULL);
 			cont->next = g_all_infos.small_mapping;
 			cont->arena_id = (cont->next) ? cont->next->arena_id + 1 : 1;
-			printf("%p %p %p\n", cont, cont->next, g_all_infos.small_mapping);
 			g_all_infos.small_mapping = cont;
-			printf("%p %p %p\n", cont, cont->next, g_all_infos.small_mapping);
-			
-			// int j = -1;
-			// t_list *li = g_all_infos.tiny_mapping;
-			// while (++j < g_all_infos.nb_tiny / 128)
-			// {
-			// 	// printf("node nb %i at %p is %p\n", j, li, li->content);
-			// 	li = li->next;
-			// }
-			// printf("node nb %i at %p is %p\n", j, li, li->content);
 		}
 		g_all_infos.small_mapping->nb_alloc += 1;
-		// printf("give tiny malloc");
-		// g_all_infos.nb_tiny += 1;
-		// printf("loaded\n");
+		g_all_infos.small_mapping->zones->content_size = size;
+		ret = g_all_infos.small_mapping->zones->content;
+		g_all_infos.small_mapping->zones = g_all_infos.small_mapping->zones->next;
+		return (ret);
 	}
-	// else if (size <= g_all_infos.small_node_size)
-	// {
-	// 	if (!(g_all_infos.small_mapping) || (g_all_infos.nb_small != 0 && g_all_infos.nb_small % 128 == 0))
-	// 	{
-	// 		printf("initiatizing small\n");
-	// 		if (!(ft_init_one_map(g_all_infos.small_size)))
-	// 			return (NULL);
-	// 		int i = -1;
-	// 		t_list *l = g_all_infos.small_mapping;
-	// 		while (++i < g_all_infos.nb_small / 128)
-	// 		{
-	// 			// printf("node nb %i at %p is %p\n", i, l, l->content);
-	// 			l = l->next;
-	// 		}
-	// 		printf("node nb %i at %p is %p\n", i, l, l->content);
-	// 	}
-	// 	g_all_infos.nb_small += 1;
-	// 	// printf("loaded\n");
-	// }
-	// else
-	// {
-	// 	printf("initiatizing big\n");
-	// 	if (!(ft_init_one_map(size)))
-	// 			return (NULL);
-	// }
+	else
+	{
+		printf("initiatizing big\n");
+	}
 	return (NULL);
+}
+
+void		print_mem(t_arena_container *zones, char *type)
+{
+	t_arena_container	*tmp;
+	t_list				*tmp_l;
+
+	// TINY : 0xA0000
+	// 0xA0020 - 0xA004A : 42 octets
+	// 0xA006A - 0xA00BE : 84 octets
+	// SMALL : 0xAD000
+	// 0xAD020 - 0xADEAD : 3725 octets
+	// LARGE : 0xB0000
+	// 0xB0020 - 0xBBEEF : 48847 octets
+	// Total : 52698 octets
+	tmp = zones;
+	while (tmp)
+	{
+		printf("%s : 0x%05X\n", type, (long)(((t_list *)tmp->first)->content) & 0xFFFFF);
+		tmp_l = tmp->zones;
+		while(tmp_l)
+		{
+			printf("0x%05X - 0x%05X : %d octets\n", (long)tmp_l->content & 0xFFFFF, (long)(tmp_l->content + tmp_l->content_size) & 0xFFFFF, tmp_l->content_size);
+			tmp_l = (tmp_l->next == tmp->first) ? NULL : tmp_l->next;
+		}
+		tmp = tmp->next;
+	}
+}
+
+void		show_alloc_mem()
+{
+	// faire le tri des zones
+	if (g_all_infos.tiny_mapping)
+		print_mem(g_all_infos.tiny_mapping, "TINY");
+	if (g_all_infos.small_mapping)
+		print_mem(g_all_infos.small_mapping, "SMALL");
 }
 
 int			main(void)
@@ -234,9 +166,11 @@ int			main(void)
 	t_arena_container *list;
 
 	i = -1;
-	while (++i <= 128 * 1280)
-		ft_malloc(0);
-	list = g_all_infos.tiny_mapping;
+	while (++i <= 128)
+		ft_malloc(i);
+	show_alloc_mem();
+		// printf("malloc nb %i at %p\n", i, ft_malloc(i));
+	// list = g_all_infos.tiny_mapping;
 	// i = 0;
 	// while (list->next)
 	// {
@@ -245,12 +179,12 @@ int			main(void)
 	// 	i++;
 	// }
 	// printf("i = %d list = %p nb_aloc = %d id = %d\n", i, list, list->nb_alloc, list->arena_id);
-	i = -1;
-	while (++i <= 170 * 128)
-		ft_malloc(150);
-		i = -1;
-	while (++i <= 170 * 128)
-		ft_malloc(1500);
+	// i = -1;
+	// while (++i <= 10 * 128)
+	// 	ft_malloc(150);
+	// i = -1;
+	// while (++i <= 170 * 128)
+	// 	ft_malloc(1500);
 	// ft_malloc(10000);
 	// ft_malloc(10000);
 	// ft_malloc(10000);
